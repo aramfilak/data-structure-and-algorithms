53911ceaeedcaa4d861c6eacc4ee8ad4bc8c5243
aramfilak <aram.filak@outlook.de>
2023-03-03 23:36:31 +0100

    update

--- algorithms/leetcode.ts
+++ algorithms/leetcode.ts
@@ -1,30 +1,21 @@
 /**
  * 1200. Minimum Absolute Difference (Easy)
  * swap two array elements.
- * Time Complexity: O(1)
+ * Time Complexity: O(n log n)
  * Space Complexity: O(1)
  */
 function minimumAbsDifference(arr: number[]): number[][] {
-	const answer: number[][] = [];
-
-	let map = new Map<number, number>();
-	// find the smallest difference.
-	let min1 = arr[0];
-	for (let i = 0; i < arr.length; i++) {
-		if (min1 > arr[i]) min1 = arr[i];
-		map.set(arr[i], arr[i]);
-	}
-
-	let min2 = arr[0] === min1 ? arr[1] : arr[0];
-	for (let i = 0; i < arr.length; i++)
-		if (min2 > arr[i] && arr[i] !== min1) min2 = arr[i];
-
-	let minDiff = Math.abs(min1 - min2);
-	for (let i = 0; i < arr.length; i++) {}
-
-	return answer;
+    const answer: number[][] = [];
+    arr.sort((a, b) => a - b);
+    let minDif = 2147483647;
+    for (let i = 1; i < arr.length; i++)
+        minDif = Math.min(minDif, arr[i] - arr[i - 1]);
+    for (let i = 1; i < arr.length; i++)
+        if (arr[i] - arr[i - 1] === minDif)
+            answer.push([arr[i - 1], arr[i]]);
+    return answer;
 }
-console.log(minimumAbsDifference([-1, -2]));
+
 
 /*
  * Terminologies:
