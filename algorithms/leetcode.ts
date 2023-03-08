10b0cf01c5cffef266797c84d3971dfe4343830c
aramfilak <aram.filak@outlook.de>
2023-03-08 01:01:02 +0100

    binary-search

--- algorithms/leetcode.ts
+++ algorithms/leetcode.ts
@@ -12,19 +12,19 @@
  * Space Complexity: O(n)
  */
 function isAnagram(s: string, t: string): boolean {
-	if (s.length !== t.length) return false;
-	let sLetters = new Map();
-	let tLetters = new Map();
-	for (let i = 0; i < s.length; i++) {
-		sLetters.set(s[i], (sLetters.get(s[i]) || 0) + 1);
-		tLetters.set(t[i], (tLetters.get(t[i]) || 0) + 1);
-	}
-	let iterator = sLetters.keys();
-	for (let i = 0; i < sLetters.size; i++) {
-		let letter = iterator.next().value;
-		if (sLetters.get(letter) !== tLetters.get(letter)) return false;
-	}
-	return true;
+    if (s.length !== t.length) return false;
+    let sLetters = new Map();
+    let tLetters = new Map();
+    for (let i = 0; i < s.length; i++) {
+        sLetters.set(s[i], (sLetters.get(s[i]) || 0) + 1);
+        tLetters.set(t[i], (tLetters.get(t[i]) || 0) + 1);
+    }
+    let iterator = sLetters.keys();
+    for (let i = 0; i < sLetters.size; i++) {
+        let letter = iterator.next().value;
+        if (sLetters.get(letter) !== tLetters.get(letter)) return false;
+    }
+    return true;
 }
 
 /***************************************************************
@@ -37,27 +37,27 @@ function isAnagram(s: string, t: string): boolean {
  * Space Complexity: O(1)
  */
 function checkXMatrix(grid: number[][]): boolean {
-	let leftDiagonalIndex: number = 0;
-	let rightDiagonalIndex: number = grid[0].length - 1;
-	for (let i = 0; i < grid.length; i++) {
-		for (let j = 0; j < grid[i].length; j++) {
-			let curPositionValue: number = grid[i][j];
-			if (
-				(leftDiagonalIndex === j && curPositionValue === 0) ||
-				(rightDiagonalIndex === j && curPositionValue === 0)
-			) {
-				return false;
-			} else if (
-				curPositionValue !== 0 &&
-				j !== leftDiagonalIndex &&
-				j !== rightDiagonalIndex
-			)
-				return false;
-		}
-		leftDiagonalIndex++;
-		rightDiagonalIndex--;
-	}
-	return true;
+    let leftDiagonalIndex: number = 0;
+    let rightDiagonalIndex: number = grid[0].length - 1;
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            let curPositionValue: number = grid[i][j];
+            if (
+                (leftDiagonalIndex === j && curPositionValue === 0) ||
+                (rightDiagonalIndex === j && curPositionValue === 0)
+            ) {
+                return false;
+            } else if (
+                curPositionValue !== 0 &&
+                j !== leftDiagonalIndex &&
+                j !== rightDiagonalIndex
+            )
+                return false;
+        }
+        leftDiagonalIndex++;
+        rightDiagonalIndex--;
+    }
+    return true;
 }
 
 /**
@@ -66,15 +66,15 @@ function checkXMatrix(grid: number[][]): boolean {
  * Space Complexity: O(n)
  */
 function transpose(matrix: number[][]): number[][] {
-	const transpose = [];
-	for (let j = 0; j < matrix[0].length; j++) {
-		let row = [];
-		for (let i = 0; i < matrix.length; i++) {
-			row.push(matrix[i][j]);
-		}
-		transpose.push(row);
-	}
-	return transpose;
+    const transpose = [];
+    for (let j = 0; j < matrix[0].length; j++) {
+        let row = [];
+        for (let i = 0; i < matrix.length; i++) {
+            row.push(matrix[i][j]);
+        }
+        transpose.push(row);
+    }
+    return transpose;
 }
 
 /**
@@ -83,42 +83,42 @@ function transpose(matrix: number[][]): number[][] {
  * Space Complexity: O(1)
  */
 function tictactoe(moves: number[][]): string {
-	const board: string[][] = [
-		['0', '0', '0'],
-		['0', '0', '0'],
-		['0', '0', '0'],
-	];
-	for (let i = 0; i < moves.length; i++) {
-		if (i % 2 === 0) board[moves[i][0]][moves[i][1]] = 'A';
-		else board[moves[i][0]][moves[i][1]] = 'B';
-	}
-	const results = [];
-	let leftDiagonalResults = [];
-	let rightDiagonal = 2;
-	let rightDiagonalResults = [];
-	for (let i = 0; i < 3; i++) {
-		let colsResults = [];
-		for (let j = 0; j < 3; j++) {
-			colsResults.push(board[j][i]);
-		}
-		results.push(colsResults);
-		leftDiagonalResults.push(board[i][i]);
-		rightDiagonalResults.push(board[i][rightDiagonal]);
-		rightDiagonal--;
-		results.push(board[i]);
-	}
-	results.push(rightDiagonalResults, leftDiagonalResults);
-	for (let i = 0; i < results.length; i++) {
-		let cur: string = results[i][0];
-		let isWinner = true;
-		for (let j = 1; j < 3; j++) {
-			if (results[i][j] !== cur) {
-				isWinner = false;
-			}
-		}
-		if (isWinner && cur !== '0') return cur;
-	}
-	return moves.length >= 9 ? 'Draw' : 'Pending';
+    const board: string[][] = [
+        ['0', '0', '0'],
+        ['0', '0', '0'],
+        ['0', '0', '0'],
+    ];
+    for (let i = 0; i < moves.length; i++) {
+        if (i % 2 === 0) board[moves[i][0]][moves[i][1]] = 'A';
+        else board[moves[i][0]][moves[i][1]] = 'B';
+    }
+    const results = [];
+    let leftDiagonalResults = [];
+    let rightDiagonal = 2;
+    let rightDiagonalResults = [];
+    for (let i = 0; i < 3; i++) {
+        let colsResults = [];
+        for (let j = 0; j < 3; j++) {
+            colsResults.push(board[j][i]);
+        }
+        results.push(colsResults);
+        leftDiagonalResults.push(board[i][i]);
+        rightDiagonalResults.push(board[i][rightDiagonal]);
+        rightDiagonal--;
+        results.push(board[i]);
+    }
+    results.push(rightDiagonalResults, leftDiagonalResults);
+    for (let i = 0; i < results.length; i++) {
+        let cur: string = results[i][0];
+        let isWinner = true;
+        for (let j = 1; j < 3; j++) {
+            if (results[i][j] !== cur) {
+                isWinner = false;
+            }
+        }
+        if (isWinner && cur !== '0') return cur;
+    }
+    return moves.length >= 9 ? 'Draw' : 'Pending';
 }
 
 /***************************************************************
@@ -131,26 +131,26 @@ function tictactoe(moves: number[][]): string {
  * Space Complexity: O(n)
  */
 function myPow(x: number, n: number): number {
-	if (x === -1) {
-		if (n >= 2147483647) return -1;
-		if (n <= -2147483648) return 1;
-	}
-	if (x === 1) if (n >= 2147483647 || n <= -2147483648) return 1;
-	if (n <= -2147483648 || n >= 2147483647) return 0;
+    if (x === -1) {
+        if (n >= 2147483647) return -1;
+        if (n <= -2147483648) return 1;
+    }
+    if (x === 1) if (n >= 2147483647 || n <= -2147483648) return 1;
+    if (n <= -2147483648 || n >= 2147483647) return 0;
 
-	if (n > 0) {
-		const power = function (base: number, exponent: number): number {
-			if (exponent === 1) return base;
-			return base * power(base, exponent - 1);
-		};
-		return power(x, n);
-	} else {
-		const power = function (base: number, exponent: number): number {
-			if (exponent === 1) return base;
-			return power(base, exponent + 1) / base;
-		};
-		return power(x, n);
-	}
+    if (n > 0) {
+        const power = function (base: number, exponent: number): number {
+            if (exponent === 1) return base;
+            return base * power(base, exponent - 1);
+        };
+        return power(x, n);
+    } else {
+        const power = function (base: number, exponent: number): number {
+            if (exponent === 1) return base;
+            return power(base, exponent + 1) / base;
+        };
+        return power(x, n);
+    }
 }
 
 /***************************************************************
@@ -163,26 +163,26 @@ function myPow(x: number, n: number): number {
  * Space Complexity: O(n)
  */
 function maxProfitAssignment(
-	difficulty: number[],
-	profit: number[],
-	worker: number[]
+    difficulty: number[],
+    profit: number[],
+    worker: number[]
 ): number {
-	const jobs: number[][] = [];
-	for (let i = 0; i < difficulty.length; i++)
-		jobs.push([difficulty[i], profit[i]]);
-	worker.sort((a, b) => a - b);
-	jobs.sort((a, b) => a[0] - b[0]);
-	let job = 0,
-		curMaxProfit = 0,
-		totalProfit = 0;
-	for (let i = 0; i < worker.length; i++) {
-		while (job < jobs.length && worker[i] >= jobs[job][0]) {
-			curMaxProfit = Math.max(curMaxProfit, jobs[job][1]);
-			job++;
-		}
-		totalProfit += curMaxProfit;
-	}
-	return totalProfit;
+    const jobs: number[][] = [];
+    for (let i = 0; i < difficulty.length; i++)
+        jobs.push([difficulty[i], profit[i]]);
+    worker.sort((a, b) => a - b);
+    jobs.sort((a, b) => a[0] - b[0]);
+    let job = 0,
+        curMaxProfit = 0,
+        totalProfit = 0;
+    for (let i = 0; i < worker.length; i++) {
+        while (job < jobs.length && worker[i] >= jobs[job][0]) {
+            curMaxProfit = Math.max(curMaxProfit, jobs[job][1]);
+            job++;
+        }
+        totalProfit += curMaxProfit;
+    }
+    return totalProfit;
 }
 
 /*
@@ -191,24 +191,24 @@ function maxProfitAssignment(
  * Space Complexity: O(1)
  */
 function largestSumAfterKNegations(nums: number[], k: number): number {
-	nums.sort((a, b) => a - b);
-	let sum = 0;
-	let minVAl = nums[0] < 0 ? Math.abs(nums[0]) : nums[0];
-	let minValIdx = 0;
-	for (let i = 0; i < nums.length; i++) {
-		if (k <= 0) break;
-		if (nums[i] < 0) {
-			nums[i] = Math.abs(nums[i]);
-			k--;
-		}
-		if (nums[i] < minVAl) {
-			minVAl = nums[i];
-			minValIdx = i;
-		}
-	}
-	if (k % 2 === 1) nums[minValIdx] = -nums[minValIdx];
-	nums.forEach(el => (sum += el));
-	return sum;
+    nums.sort((a, b) => a - b);
+    let sum = 0;
+    let minVAl = nums[0] < 0 ? Math.abs(nums[0]) : nums[0];
+    let minValIdx = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (k <= 0) break;
+        if (nums[i] < 0) {
+            nums[i] = Math.abs(nums[i]);
+            k--;
+        }
+        if (nums[i] < minVAl) {
+            minVAl = nums[i];
+            minValIdx = i;
+        }
+    }
+    if (k % 2 === 1) nums[minValIdx] = -nums[minValIdx];
+    nums.forEach(el => (sum += el));
+    return sum;
 }
 
 /**
@@ -217,10 +217,10 @@ function largestSumAfterKNegations(nums: number[], k: number): number {
  * Space Complexity: O(1)
  */
 function arrayPairSum(nums: number[]): number {
-	nums.sort((a, b) => a - b);
-	let maximizedSum: number = 0;
-	for (let i = 0; i < nums.length; i += 2) maximizedSum += nums[i];
-	return maximizedSum;
+    nums.sort((a, b) => a - b);
+    let maximizedSum: number = 0;
+    for (let i = 0; i < nums.length; i += 2) maximizedSum += nums[i];
+    return maximizedSum;
 }
 
 /**
@@ -229,14 +229,14 @@ function arrayPairSum(nums: number[]): number {
  * Space Complexity: O(1)
  */
 function largestPerimeter(nums: number[]): number {
-	nums.sort((a, b) => a - b);
-	for (let i = nums.length - 1; i > 1; i--) {
-		let a: number = nums[i - 2],
-			b: number = nums[i - 1],
-			c: number = nums[i];
-		if (a + b > c) return a + b + c;
-	}
-	return 0;
+    nums.sort((a, b) => a - b);
+    for (let i = nums.length - 1; i > 1; i--) {
+        let a: number = nums[i - 2],
+            b: number = nums[i - 1],
+            c: number = nums[i];
+        if (a + b > c) return a + b + c;
+    }
+    return 0;
 }
 
 /**
@@ -245,12 +245,12 @@ function largestPerimeter(nums: number[]): number {
  * Space Complexity: O(1)
  */
 function minimumAbsDifference(arr: number[]): number[][] {
-	const answer: number[][] = [];
-	arr.sort((a, b) => a - b);
-	let minDif: number = 2147483647;
-	for (let i = 1; i < arr.length; i++)
-		minDif = Math.min(minDif, arr[i] - arr[i - 1]);
-	for (let i = 1; i < arr.length; i++)
-		if (arr[i] - arr[i - 1] === minDif) answer.push([arr[i - 1], arr[i]]);
-	return answer;
+    const answer: number[][] = [];
+    arr.sort((a, b) => a - b);
+    let minDif: number = 2147483647;
+    for (let i = 1; i < arr.length; i++)
+        minDif = Math.min(minDif, arr[i] - arr[i - 1]);
+    for (let i = 1; i < arr.length; i++)
+        if (arr[i] - arr[i - 1] === minDif) answer.push([arr[i - 1], arr[i]]);
+    return answer;
 }
