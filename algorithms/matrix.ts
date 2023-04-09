94beda9189fb2a9e3925f19718b8e7d2b5d5cb87
aramfilak <aram.filak@outlook.de>
2023-04-09 16:22:23 +0200

    54. Spiral Matrix

--- algorithms/matrix.ts
+++ algorithms/matrix.ts
@@ -5,12 +5,12 @@
  * Space Complexity: O(1)
  */
 function diagonalSum(mat: number[][]): number {
-  let sum: number = 0;
-  for (let i = 0, j = mat.length - 1; i < mat.length; i++, j--) {
-    sum += mat[i][i]
-    if (i !== j) sum += mat[i][j];
-  }
-  return sum;
+	let sum: number = 0;
+	for (let i = 0, j = mat.length - 1; i < mat.length; i++, j--) {
+		sum += mat[i][i];
+		if (i !== j) sum += mat[i][j];
+	}
+	return sum;
 }
 
 /**
@@ -20,27 +20,23 @@ function diagonalSum(mat: number[][]): number {
  * Space Complexity: O(1)
  */
 function checkXMatrix(grid: number[][]): boolean {
-  let leftDiagonalIndex: number = 0;
-  let rightDiagonalIndex: number = grid[0].length - 1;
-  for (let i = 0; i < grid.length; i++) {
-    for (let j = 0; j < grid[i].length; j++) {
-      let curPositionValue: number = grid[i][j];
-      if (
-        (leftDiagonalIndex === j && curPositionValue === 0) ||
-        (rightDiagonalIndex === j && curPositionValue === 0)
-      ) {
-        return false;
-      } else if (
-        curPositionValue !== 0 &&
-        j !== leftDiagonalIndex &&
-        j !== rightDiagonalIndex
-      )
-        return false;
-    }
-    leftDiagonalIndex++;
-    rightDiagonalIndex--;
-  }
-  return true;
+	let leftDiagonalIndex: number = 0;
+	let rightDiagonalIndex: number = grid[0].length - 1;
+	for (let i = 0; i < grid.length; i++) {
+		for (let j = 0; j < grid[i].length; j++) {
+			let curPositionValue: number = grid[i][j];
+			if (
+				(leftDiagonalIndex === j && curPositionValue === 0) ||
+				(rightDiagonalIndex === j && curPositionValue === 0)
+			) {
+				return false;
+			} else if (curPositionValue !== 0 && j !== leftDiagonalIndex && j !== rightDiagonalIndex)
+				return false;
+		}
+		leftDiagonalIndex++;
+		rightDiagonalIndex--;
+	}
+	return true;
 }
 
 /**
@@ -49,61 +45,43 @@ function checkXMatrix(grid: number[][]): boolean {
  * Time Complexity: O(n)
  * Space Complexity: O(n)
  */
-function transpose(matrix: number[][]): number[][] {
-  const transpose = [];
-  for (let j = 0; j < matrix[0].length; j++) {
-    let row = [];
-    for (let i = 0; i < matrix.length; i++) {
-      row.push(matrix[i][j]);
-    }
-    transpose.push(row);
-  }
-  return transpose;
-}
-
-/**
- * LeetCode Problem (Easy):
- * 1275. Find Winner on a Tic Tac Toe Game
- * Time Complexity: O(1)
- * Space Complexity: O(1)
- */
 function tictactoe(moves: number[][]): string {
-  const board: string[][] = [
-    ["0", "0", "0"],
-    ["0", "0", "0"],
-    ["0", "0", "0"],
-  ];
-  for (let i = 0; i < moves.length; i++) {
-    if (i % 2 === 0) board[moves[i][0]][moves[i][1]] = "A";
-    else board[moves[i][0]][moves[i][1]] = "B";
-  }
-  const results = [];
-  let leftDiagonalResults = [];
-  let rightDiagonal = 2;
-  let rightDiagonalResults = [];
-  for (let i = 0; i < 3; i++) {
-    let colsResults = [];
-    for (let j = 0; j < 3; j++) {
-      colsResults.push(board[j][i]);
-    }
-    results.push(colsResults);
-    leftDiagonalResults.push(board[i][i]);
-    rightDiagonalResults.push(board[i][rightDiagonal]);
-    rightDiagonal--;
-    results.push(board[i]);
-  }
-  results.push(rightDiagonalResults, leftDiagonalResults);
-  for (let i = 0; i < results.length; i++) {
-    let cur: string = results[i][0];
-    let isWinner = true;
-    for (let j = 1; j < 3; j++) {
-      if (results[i][j] !== cur) {
-        isWinner = false;
-      }
-    }
-    if (isWinner && cur !== "0") return cur;
-  }
-  return moves.length >= 9 ? "Draw" : "Pending";
+	const board: string[][] = [
+		["0", "0", "0"],
+		["0", "0", "0"],
+		["0", "0", "0"],
+	];
+	for (let i: number = 0; i < moves.length; i++) {
+		if (i % 2 === 0) board[moves[i][0]][moves[i][1]] = "A";
+		else board[moves[i][0]][moves[i][1]] = "B";
+	}
+	const results: string[][] = [];
+	let leftDiagonalResults: string[] = [];
+	let rightDiagonal: number = 2;
+	let rightDiagonalResults: string[] = [];
+	for (let i: number = 0; i < 3; i++) {
+		let colsResults: string[] = [];
+		for (let j: number = 0; j < 3; j++) {
+			colsResults.push(board[j][i]);
+		}
+		results.push(colsResults);
+		leftDiagonalResults.push(board[i][i]);
+		rightDiagonalResults.push(board[i][rightDiagonal]);
+		rightDiagonal--;
+		results.push(board[i]);
+	}
+	results.push(rightDiagonalResults, leftDiagonalResults);
+	for (let i: number = 0; i < results.length; i++) {
+		let cur: string = results[i][0];
+		let isWinner: boolean = true;
+		for (let j: number = 1; j < 3; j++) {
+			if (results[i][j] !== cur) {
+				isWinner = false;
+			}
+		}
+		if (isWinner && cur !== "0") return cur;
+	}
+	return moves.length >= 9 ? "Draw" : "Pending";
 }
 
 /**
@@ -113,18 +91,18 @@ function tictactoe(moves: number[][]): string {
  * Space Complexity: O(1)
  */
 function deleteGreatestValue(grid: number[][]): number {
-  for (const gridElement of grid) {
-    gridElement.sort((a, b) => a - b);
-  }
-  let ans = 0;
-  for (let j = 0; j < grid[0].length; j++) {
-    let max = Number.MIN_VALUE;
-    for (let k = 0; k < grid.length; k++) {
-      if (max < grid[k][j]) max = grid[k][j];
-    }
-    ans += max;
-  }
-  return ans;
+	for (const gridElement of grid) {
+		gridElement.sort((a, b) => a - b);
+	}
+	let ans = 0;
+	for (let j = 0; j < grid[0].length; j++) {
+		let max = Number.MIN_VALUE;
+		for (let k = 0; k < grid.length; k++) {
+			if (max < grid[k][j]) max = grid[k][j];
+		}
+		ans += max;
+	}
+	return ans;
 }
 
 /**
@@ -134,18 +112,68 @@ function deleteGreatestValue(grid: number[][]): number {
  * Space Complexity: O(m*n)
  */
 function matrixReshape(mat: number[][], r: number, c: number): number[][] {
-  if (mat.length * mat[0].length !== c * r) return mat;
-  let m = 0, row: number[] = [], ans: number[][] = [];
-  for (let i = 0; i < mat.length; i++) {
-    for (let j = 0; j < mat[0].length; j++) {
-      row[m] = mat[i][j];
-      m++;
-      if (m === c) {
-        ans.push(row);
-        row = [];
-        m = 0;
-      }
-    }
-  }
-  return ans;
+	if (mat.length * mat[0].length !== c * r) return mat;
+	let m = 0,
+		row: number[] = [],
+		ans: number[][] = [];
+	for (let i = 0; i < mat.length; i++) {
+		for (let j = 0; j < mat[0].length; j++) {
+			row[m] = mat[i][j];
+			m++;
+			if (m === c) {
+				ans.push(row);
+				row = [];
+				m = 0;
+			}
+		}
+	}
+	return ans;
+}
+/**
+ * LeetCode Problem (Medium):
+ * 54. Spiral Matrix
+ * Time Complexity: O(m*n)
+ * Space Complexity: O(m*n)
+ */
+function spiralOrder(matrix: number[][]): number[] {
+	const ans: number[] = [],
+		visited: Set<string> = new Set<string>(),
+		directionQueue = [
+			[0, 1], // right
+			[1, 0], // bottom
+			[0, -1], // left
+			[-1, 0], //top
+		];
+
+	let lastPosition = [0, -1];
+	let matrixElements = matrix.length * matrix[0].length;
+
+	while (true) {
+		let direction: number[] = directionQueue.shift()!;
+		lastPosition[0] += direction[0];
+		lastPosition[1] += direction[1];
+		lastPosition = drive(lastPosition, direction!);
+		if (ans.length === matrixElements) break;
+		directionQueue.push(direction!);
+	}
+
+	return ans;
+
+	function drive(lastPosition: number[], direction: number[]): number[] {
+		const yDir = direction[0];
+		const xDir = direction[1];
+		for (
+			let i = lastPosition[0], j = lastPosition[1];
+			i < matrix.length && i > -1 && j < matrix[i].length && j > -1;
+			i += yDir, j += xDir
+		) {
+			const key = `${i}+${j}`;
+			if (visited.has(key)) break;
+			lastPosition = [i, j];
+			ans.push(matrix[i][j]);
+			visited.add(key);
+		}
+
+		return lastPosition;
+	}
 }
